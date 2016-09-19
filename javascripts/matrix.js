50ae05e2d287cc5b4691b1786d82209781d55733
vmarquet <vincent.marquet1@free.fr>
2016-09-19 02:27:49 +0200

    rework matrix effect algorithm, use full screen

--- javascripts/matrix.js
+++ javascripts/matrix.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-  // set canvas to same size that header
-  canvas.style.width = '100%';
-  canvas.style.height = '100%';
-  canvas.width  = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
+  canvas.width  = window.screen.width;
+  canvas.height = window.screen.height;
 
   var width = canvas.width;
   var height = canvas.height;
   var numberOfColumns = width / 10;
-  var yPositions = Array(numberOfColumns).join(0).split('');
+  var yPositions = Array(numberOfColumns);
+  for (var i=0; i<yPositions.length; i++) {
+    yPositions[i] = {y: 0};
+  }
   var ctx = canvas.getContext('2d');
+  ctx.font = '10pt Georgia';
 
   var alpha = 0.05;
   var fade = function () {
     alpha += 0.05;
-    //console.log(alpha);
   }
 
   var startFading = function () {
@@ -29,26 +29,65 @@ $(document).ready(function() {
   }
    
   var draw = function () {
-    // darken already printed characters
-    ctx.fillStyle = 'rgba(24,24,24,' + alpha + ')';
-    ctx.fillRect(0, 0, width, height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // write new characters
-    ctx.fillStyle = 'rgba(0,255,0,' + (1-alpha) + ')'; // green
-    ctx.font = '10pt Georgia';
-    yPositions.map(function(y, index) {
-      // get random char
-      text = String.fromCharCode(100 + Math.random()*33);
+    for (var index=0; index<yPositions.length; index++) {
+      y = yPositions[index];
+
+      // check if there's already a column of chars at that x offset
+      if (y['y'] === 0) {
+        // if no column of chars, we choose whether create one or not
+        var rand = Math.random();
+        if (rand > 0.005)
+          continue;
+
+        // we create one, so we have to select a set of characters
+        rand = Math.random();
+        if (rand < 0.2)
+          y['charset'] = '01';
+        else if (rand < 0.4)
+          y['charset'] = 'jap';
+        else
+          y['charset'] = 'ascii';
+
+        y['chars'] = [];
+      }
+
+      // we create a new char
+      if (y['charset'] === '01') {
+        var rand = Math.random();
+        if (rand < 0.5)
+          y['chars'].unshift('0');
+        else
+          y['chars'].unshift('1');
+      }
+      if (y['charset'] === 'jap') {
+        var jap = 'あいうえおぎくけこさしすせそたちつてとにののふべまもよりれろんウオカコシスソツテホ';
+        var n = Math.floor(Math.random()*jap.length);
+        y['chars'].unshift(jap[n]);
+      }
+      if (y['charset'] === 'ascii') {
+        y['chars'].unshift(String.fromCharCode(100 + Math.random()*33))
+      }
+
+      // we limit column size
+      if (y['chars'].length > 30) {
+        y['chars'].splice(-1,1);
+      }
 
       x = (index * 10) + 10;
-      canvas.getContext('2d').fillText(text, x, y);
+      for (var i=0; i<y['chars'].length; i++) {
+        var alpha_local = i*0.03;
+        ctx.fillStyle = 'rgba(0,255,0,' + (1 - alpha_local - alpha) + ')'; // green
+        canvas.getContext('2d').fillText(y['chars'][i], x, y['y']-i*10);
+      }
 
       if(y > 100 + Math.random()*1e4) {
-        yPositions[index] = 0; // restart from top
+        y['y'] = 0; // restart from top
       } else {
-        yPositions[index] = y + 10; // next character 10 pixels down
+        y['y'] = y['y'] + 10; // next character 10 pixels down
       }
-    });
+    };
   };
 
   RunMatrix();
